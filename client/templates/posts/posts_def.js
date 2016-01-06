b797b688c7be6f245da29c304edcde2c89dfd119
kingshark <kingshark8848@live.com>
2016-01-06 18:21:51 +1100

    modified:   .meteor/packages 	modified:   .meteor/versions 	modified:   client/main.html 	deleted:    client/main.js 	new file:   client/templates/app/layout.html 	new file:   client/templates/app/not_found.html 	new file:   client/templates/loading.html 	modified:   client/templates/posts/post_item.html 	new file:   client/templates/posts/post_page.html 	renamed:    client/templates/posts/post_item.js -> client/templates/posts/posts_def.js 	deleted:    client/templates/posts/posts_list.js 	new file:   lib/router.js 	deleted:    meteor_kingshark1.css 	deleted:    meteor_kingshark1.html 	deleted:    meteor_kingshark1.js

--- client/templates/posts/posts_def.js
+++ client/templates/posts/posts_def.js
@@ -1,3 +1,9 @@
+Template.postsList.helpers({
+  posts: function() {
+    return Posts.find();
+  }
+});
+
 Template.postItem.helpers({
   domain: function() {
     var a = document.createElement('a');

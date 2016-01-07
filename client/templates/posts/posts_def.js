f3499fc8ac2866ac11b0e6c43a12c3388aa4a208
kingshark <kingshark8848@live.com>
2016-01-07 18:16:13 +1100

    +post submit

--- client/templates/posts/posts_def.js
+++ client/templates/posts/posts_def.js
@@ -11,3 +11,19 @@ Template.postItem.helpers({
     return a.hostname;
   }
 });
+
+Template.postSubmit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var post = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    };
+
+    Posts.insert(post);
+    post.postid=Posts.findOne(post)._id;
+    Router.go('postPage', post);
+    //console.log(post._id);
+  }
+});
